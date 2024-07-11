64ed5668592313822dfac9273f705d3d8eb4378c
tejakonduru12 <tejakonduru2003@gmail.com>
2024-07-11 14:41:28 +0530

    Add files via upload

---
+++ src/components/Question.js
@@ -0,0 +1,17 @@
+// src/components/Question.js
+import React from 'react';
+const Question = ({ question, options, handleAnswer }) => {
+  return (
+    <div className="question">
+      <h2>{question}</h2>
+      <div className="options">
+        {options.map((option, index) => (
+          <button key={index} onClick={() => handleAnswer(option)}>
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Question;
